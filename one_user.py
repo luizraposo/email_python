edc3ba2d9aae712554b96dd02d8d0edb7ce22a21
luizraposo <63749950+luizraposo@users.noreply.github.com>
2020-06-02 15:42:41 -0300

    Create one_user.py

---
+++ one_user.py
@@ -0,0 +1,33 @@
+from string import Template
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+
+meu_email = ''
+minha_senha = ''
+
+with open('corpo.html', 'r') as html:
+    template = Template(html.read())
+    corpo_msg = template.substitute()
+
+recipients = '#CLIENTE'
+msg = MIMEMultipart()
+msg['from'] = ''
+msg['to'] = recipients  #CLIENTE
+msg['subject'] = 'Assunto'
+
+corpo = MIMEText('')
+msg.attach(corpo)
+
+with smtplib.SMTP('smtp.dc1.buy4.io:587') as smtp:
+    try:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.login(meu_email, minha_senha)
+        smtp.send_message(msg)
+
+        print('E-mail enviado com sucesso.')
+    except Exception as e:
+        print('E-mail não enviado...')
+        print('Erro:', e)
